85973e492c6b692519e298e9d5ad41881d41d873
Chun Lok Lau <lauchunlok1@gmail.com>
2022-07-27 22:57:47 +0100

    Updates

---
+++ precache-manifest.df5b656760b1d51add485693147bc19c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1546218bfecce397a943",
+    "url": "/react_solo_1-digital-card/static/css/main.8c9bf6a7.chunk.css"
+  },
+  {
+    "revision": "1546218bfecce397a943",
+    "url": "/react_solo_1-digital-card/static/js/main.1546218b.chunk.js"
+  },
+  {
+    "revision": "7d8a5b69bced8be18895",
+    "url": "/react_solo_1-digital-card/static/js/1.7d8a5b69.chunk.js"
+  },
+  {
+    "revision": "7d89464741f2193926da",
+    "url": "/react_solo_1-digital-card/static/js/2.7d894647.chunk.js"
+  },
+  {
+    "revision": "c43506a2521c0fe93c3f",
+    "url": "/react_solo_1-digital-card/static/js/runtime~main.c43506a2.js"
+  },
+  {
+    "revision": "1a554d388c07d72ac13c3f7f75c8d490",
+    "url": "/react_solo_1-digital-card/static/media/Photo.1a554d38.png"
+  },
+  {
+    "revision": "f2245c0315cd1db7254ddd985d979a5f",
+    "url": "/react_solo_1-digital-card/index.html"
+  }
+];
